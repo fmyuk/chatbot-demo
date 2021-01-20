e677fd2dea78d09cb123bec2a3ae218e58e28724
fumi uk <dcfumiya.238@gmail.com>
2021-01-20 15:35:39 +0900

    chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import defaultDataset from "./dataset";
 import "./assets/styles/style.css";
+import { AnswersList } from "./components/index";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,11 +16,23 @@ export default class App extends React.Component {
     }
   }
 
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+    this.setState({
+      answers: initAnswers
+    })
+  }
+
+  componentDidMount() {
+    this.initAnswer()
+  }
+
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          {this.state.currentId}
+          <AnswersList answers={this.state.answers} />
         </div>
       </section>
     );
